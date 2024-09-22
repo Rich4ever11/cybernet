941362fb2236cfe5441c9aebae8b864f4e9a7d88
Rich4ever11 <rich4ever11@protonmail.com>
2024-09-21 20:59:09 -0400

    added dynamic file adding

--- components/RedTeam/AI/Document.js
+++ components/RedTeam/AI/Document.js
@@ -1,25 +1,44 @@
 import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import dynamic from "next/dynamic";
+import "react-pdf/dist/Page/TextLayer.css";
 
 // const PdfViewerComponent = dynamic(() => import("react-doc-viewer"), {
 //   ssr: false,
 // });
 
 export const DocumentViewer = ({}) => {
-  const docs = [
+  const [dynamicDocs, setDynamicDocs] = useState([
     {
       uri: "/UnofficialTranscript.pdf", // for remote file
     },
-  ];
+  ]);
+
+  const handleFileUpload = (event) => {
+    console.log(event.target.files[0]);
+    const objectURL = URL.createObjectURL(event.target.files[0]);
+    setDynamicDocs([
+      ...dynamicDocs,
+      {
+        uri: objectURL,
+      },
+    ]);
+  };
 
   return (
     <>
       <div>
         <div style={{ height: "100vh", overflow: "hidden" }} className="p-2">
+          <div>
+            <input
+              type="file"
+              className="file-input file-input-ghost w-full max-w-xs"
+              onChange={handleFileUpload}
+            />
+          </div>
           <DocViewer
             pluginRenderers={DocViewerRenderers}
-            documents={docs}
+            documents={dynamicDocs}
             style={{ maxHeight: "100vh", maxWidth: "100%", overflow: "scroll" }}
           />
         </div>
