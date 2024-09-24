a4e87857a2edc4623837cce70a501c0539ea7649
Rich4ever11 <rich4ever11@protonmail.com>
2024-09-24 16:41:44 -0400

    added UI for exploit search

--- app/red-team-tools/page.tsx
+++ app/red-team-tools/page.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Reconnaissance from "@/components/RedTeam/Tools/Recon/Reconnaissance";
 import Scanning from "@/components/RedTeam/Tools/Scanning/Scanning";
+import Exploit from "@/components/RedTeam/Tools/Exploit/Exploit";
 
 type Props = {};
 
 export default function Recon({}: Props) {
   return (
     <div>
-      {/* <Reconnaissance /> */}
-      <Scanning />
+      {/* <Reconnaissance />
+      <Scanning /> */}
+      <Exploit />
     </div>
   );
 }
