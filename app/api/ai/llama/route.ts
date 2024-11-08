266740e38d76129bccda77c32319f8b4101ca4e2
Rich4ever11 <rich4ever11@protonmail.com>
2024-11-08 13:59:13 -0500

    added error handling for lamma route

--- app/api/ai/llama/route.ts
+++ app/api/ai/llama/route.ts
@@ -5,15 +5,20 @@ import { HfInference } from "@huggingface/inference";
 const inference = new HfInference(process.env.HUGGING_FACE_API_KEY);
 
 export async function POST(req: NextApiRequest, _: NextApiResponse) {
-  const { question } = await new Response(req.body).json();
+  try {
+    const { question } = await new Response(req.body).json();
 
-  const result = await inference.chatCompletion({
-    model: "mistralai/Mistral-Nemo-Instruct-2407",
-    messages: [{ role: "user", content: question }],
-    max_tokens: 500,
-  });
-  return NextResponse.json(
-    { body: result.choices[0].message },
-    { status: 200 }
-  );
+    const result = await inference.chatCompletion({
+      model: "mistralai/Mistral-Nemo-Instruct-2407",
+      messages: [{ role: "user", content: question }],
+      max_tokens: 500,
+    });
+    return NextResponse.json(
+      { body: result.choices[0].message },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error_message: error }, { status: 400 });
+  }
 }
